15bd6b5ba9434facca692ee746dc7c73a1c0132a
akshatbatra10 <62565788+akshatbatra10@users.noreply.github.com>
2023-02-04 23:19:51 +0530

    created redux store and form component

---
+++ client/src/components/Posts/style.js
@@ -0,0 +1,16 @@
+import { makeStyles } from "tss-react/mui";
+
+const useStyles = makeStyles()((theme) => ({
+  mainContainer: {
+    display: "flex",
+    alignItems: "center",
+  },
+  smMargin: {
+    margin: theme.spacing(1),
+  },
+  actionDiv: {
+    textAlign: "center",
+  },
+}));
+
+export default useStyles;
