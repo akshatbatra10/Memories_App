1173d6e52f73e372d49cdd7515474142f649c4d8
akshatbatra10 <62565788+akshatbatra10@users.noreply.github.com>
2023-02-04 14:03:35 +0530

    setup the backend server

--- server/index.js
+++ server/index.js
@@ -1,4 +1,27 @@
+import * as dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors);
+
+const DATABASE_URL = process.env.DATABASE_URL;
+const PORT = process.env.PORT || 5000;
+
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(DATABASE_URL)
+  .then(() =>
+    app.listen(PORT, () => {
+      console.log(`Server running on port: ${PORT}`);
+    })
+  )
+  .catch((err) => {
+    console.log(err.message);
+  });
